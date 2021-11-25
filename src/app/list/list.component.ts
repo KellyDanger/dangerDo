715c87a9209540249203b5b54ae9be8f854f8218
Kelly Danger <kellyanndanger@gmail.com>
2021-11-25 14:10:29 -0600

    added table to list template, and added task interface

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../task';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['../app.component.css']
 })
 export class ListComponent implements OnInit {
+
+  task: Task = {
+    id: 1,
+    title: 'Make a Duck',
+    done: false
+  }
+
 
   constructor() { }
 
